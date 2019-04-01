24163b38af7690a302201815c55a8d327fc7428e
John Elam <elam.john@pwc.com>
2019-04-01 15:18:51 -0500

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to prop-trading
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/prop_trading/core_test.clj
@@ -0,0 +1,7 @@
+(ns prop-trading.core-test
+  (:require [clojure.test :refer :all]
+            [prop-trading.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
